b4761e22352d9233d149f703e40f9b46309fa92c
Taylor Marshall <taylor.paul.marshall@gmail.com>
2013-01-26 23:40:39 -0800

    add plugin

---
+++ CopyFirstRegion/copy_first_region.py
@@ -0,0 +1,15 @@
+import sublime
+import sublime_plugin
+
+
+class CopyFirstRegionCommand(sublime_plugin.TextCommand):
+  """Copies the first region selected by the user.
+
+  First is considered to be the region that occurs earliest in the file.
+  """
+
+  def run(self, edit):
+    for region in self.view.sel():
+      if not region.empty():
+        sublime.set_clipboard(self.view.substr(region))
+        return
